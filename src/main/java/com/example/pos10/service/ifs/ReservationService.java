a8ebbfff95ddd64c1aa38db3ef0c11f0670aae07
chien pei <com2100927@gmail.com>
2024-10-12 12:06:26 +0800

    Reservation 手動取消、報到、自動取消、訂位信件確定、提醒皆完成

--- src/main/java/com/example/pos10/service/ifs/ReservationService.java
+++ src/main/java/com/example/pos10/service/ifs/ReservationService.java
@@ -10,27 +10,30 @@ import com.example.pos10.vo.ReservationRes;
 public interface ReservationService {
 	
 	// 1. 查詢可用桌位並儲存
-	public ReservationRes generateAndFindAvailableTables(LocalDate reservationDate, int diningDuration, int reservationPeople);
+	public ReservationRes generateAndFindAvailableTables (LocalDate reservationDate, int diningDuration, int reservationPeople);
 
 	// 2. 計算可用的開始時間段
-	public List<LocalTime> calculateAvailableStartTimes(LocalTime openingTime, LocalTime closingTime, int diningDuration);
+	public List <LocalTime> calculateAvailableStartTimes (LocalTime openingTime, LocalTime closingTime, int diningDuration);
     
     // 3. 儲存訂位
     public ReservationRes saveReservation (ReservationReq reservationReq);
-//
-//    // 2. 根據顧客電話號碼查詢訂位
-//    public ReservationRes findReservationsByPhoneNumber (String phoneNumber);
 
-    // 3. 查詢當天的所有訂位
+    // 4. 根據顧客電話號碼查詢訂位
+    public ReservationRes findReservationsByPhoneNumber (String phoneNumber);
+
+    // 5. 查詢當天的所有訂位
     public ReservationRes findReservationsByDate (LocalDate reservationDate);
 
-    // 4. 取消訂位
+    // 6. 手動取消訂位
     public ReservationRes cancelReservation (int reservationId);
-//
-//    // 5. 自動更新桌位狀態
-//    public void autoUpdateTableStatus (LocalDate currentDate, LocalTime currentTime, LocalTime cutOffTime);
-//    
-//    // 6. 訂位前一天發送提醒
-//    public void sendReservationReminders ();
-//    
+
+    // 7. 自動更新桌位狀態（每10分鐘執行一次）
+    public ReservationRes autoUpdateTableStatus (LocalDate currentDate, LocalTime currentTime);
+
+    // 8. 手動報到更新桌位狀態
+    public ReservationRes manualCheckIn (String tableNumber);
+    
+    // 9. 訂位前一天發送提醒
+    public void sendReservationReminders ();
+    
 }
